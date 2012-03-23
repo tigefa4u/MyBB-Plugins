22eabfb118df1b3c5d4eb5fbaacce8298bd2433f
MattRogowski <mmm3567@gmail.com>
2012-03-23 20:53:01 +0000

    Add Karma Stars and Base Plugin

---
+++ inc/plugins/baseplugin.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Base Plugin 0.1
+
+ * Copyright 2011 Matthew Rogowski
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ ** http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+
+
+
+function baseplugin_info()
+{
+	return array(
+		"name" => "Base Plugin",
+		"description" => "Description of the base plugin.",
+		"website" => "http://mattrogowski.co.uk/mybb/",
+		"author" => "MattRogowski",
+		"authorsite" => "http://mattrogowski.co.uk/mybb/",
+		"version" => "0.1",
+		"compatibility" => "16*",
+		"guid" => ""
+	);
+}
+
+function baseplugin_install()
+{
+	
+}
+
+function baseplugin_is_installed()
+{
+	
+}
+
+function baseplugin_uninstall()
+{
+	
+}
+
+function baseplugin_activate()
+{
+	
+}
+
+function baseplugin_deactivate()
+{
+	
+}
+?>
